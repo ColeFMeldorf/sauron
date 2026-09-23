f06b21f16a99eca731e44f21e62a5b919c4809fc
Cole Foxon Meldorf <colefmeldorf@gmail.com>
2026-09-23 12:37:48 -0500

    seemed to work

--- sauron.py
+++ sauron.py
@@ -34,7 +34,7 @@ def main():
                         " that simulations look reasonable.", default=True)
     parser.add_argument("--fit-only-one-combined", "--fit1", action=argparse.BooleanOptionalAction, help="Only fit one combined dataset across all"
                         " surveys, instead of fitting as many as there are datasets. I.e., if I have 5"
-                        "simulated datasets and 10 for another, I could do 5 combined datasets if this is set to False.", default=True)
+                        "simulated datasets and 10 for another, I could do 5 combined datasets if this is set to False.", default=False)
     args = parser.parse_args()
 
     runner = sauron_runner(args)
